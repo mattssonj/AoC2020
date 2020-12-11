be01da2ce5f9ca9d646c98d47d1e57596372b35f
Joakim Mattsson <joakim.mattsson@tacdis.com>
2020-12-11 09:30:35 +0100

    Day 11

--- src/main/kotlin/day/Day11.kt
+++ src/main/kotlin/day/Day11.kt
@@ -6,7 +6,7 @@ import kotlin.math.max
 class Day11(private val dirtyInput: List<String>) : Solver<Int> {
 
     override fun calculateFirst(): Int {
-        val model = Model(dirtyInput)
+        val model = Model(dirtyInput, shouldBeOccupiedPart1)
         do {
             val changes = model.stabilize()
         } while (changes != 0)
@@ -14,90 +14,129 @@ class Day11(private val dirtyInput: List<String>) : Solver<Int> {
     }
 
     override fun calculateSecond(): Int {
-        return 0
+        val model = Model(dirtyInput, shouldBeOccupiedPart2)
+        do {
+            val changes = model.stabilize()
+        } while (changes != 0)
+        return model.getNumberOfOccupiedSeats()
     }
 }
 
-class Model(input: List<String>) {
+class Model(input: List<String>, private val shouldBeOccupied: (Seat, WaitingArea) -> Boolean) {
 
-    private val width = input.first().length
-    private val height = input.size
-    private val floor = input.map { it.toCharArray() }
+    private var currentStateOfWaitingArea = WaitingArea(input)
 
     fun stabilize(): NumberOfChanges {
-        var numberOfChanges = 0
-        floor.indices.forEach { y ->
-            floor[y].indices.forEach { x ->
-                val currentPos = Position(x, y)
-                val currentSeatValue = getSeat(currentPos)
-                val nextSeatValue =
-                        when {
-                            currentSeatValue == Floor -> currentSeatValue
-                            isHappy(currentPos) -> Occupied
-                            else -> EmptySeat
-                        }
-                /*if (isHappy(currentPos)) Occupied
-                else if (currentSeatValue == Floor) currentSeatValue
-                else EmptySeat*/
-                if (currentSeatValue != nextSeatValue) numberOfChanges++
-                floor[currentPos.y][currentPos.x] = nextSeatValue
-            }
-        }
-        printCurrentState()
-        return numberOfChanges
+        val currentlyOccupiedSeats = currentStateOfWaitingArea.getNumberOfOccupiedSeats()
+        val seatsToBeOccupied = currentStateOfWaitingArea.getAllPossibleSeats()
+            .filter { shouldBeOccupied(it, currentStateOfWaitingArea) }
+            .toSet()
+
+        currentStateOfWaitingArea.occupyOnly(seatsToBeOccupied)
+
+        return currentlyOccupiedSeats - seatsToBeOccupied.size
     }
 
     fun getNumberOfOccupiedSeats(): Int {
-        var occupied = 0
-        floor.forEach {
-            it.forEach {
-                if (it == Occupied) occupied++
+        return currentStateOfWaitingArea.getNumberOfOccupiedSeats()
+    }
+
+}
+
+class WaitingArea(initialValue: List<String>) {
+
+    private var floor = initialValue.map { it.toCharArray() }
+
+    fun getAllPossibleSeats(): Set<Seat> {
+        val possibleSeats = mutableSetOf<Seat>()
+        floor.forEachIndexed { y, row ->
+            row.forEachIndexed { x, seat ->
+                val pos = Position(x, y)
+                if (seat != Floor) possibleSeats.add(Seat(pos, getValueOfPosition(pos)))
             }
         }
-        return occupied
+        return possibleSeats
     }
 
-    private fun isHappy(seatPos: Position): Boolean {
-        if (floor[seatPos.y][seatPos.x] == EmptySeat) return true
-        return calculateOccupiedNeighbours(seatPos) >= 4
-    }
-
-    private fun calculateOccupiedNeighbours(seatPos: Position): Int {
-        var occupied = 0
-        findNeighbours(seatPos).forEach {
-            if (floor[it.y][it.x] == Occupied) occupied++
-        }
-        return occupied
-    }
-
-    private fun findNeighbours(current: Position): List<Position> {
-        val neighbours = mutableListOf<Position>()
-        (max(0, current.y - 1)..min(height - 1, current.y + 1)).forEach { y ->
-            (max(0, current.x - 1)..min(width - 1, current.x + 1)).forEach { x ->
-                val pos = Position(x, y)
-                if (pos != current) neighbours.add(pos)
+    fun findNeighbours(pos: Position): Set<Seat> {
+        val neighbours = mutableSetOf<Seat>()
+        (max(0, pos.y - 1)..min(floor.size - 1, pos.y + 1)).forEach { y ->
+            (max(0, pos.x - 1)..min(floor.first().size - 1, pos.x + 1)).forEach { x ->
+                val neighbourPos = Position(x, y)
+                if (pos != neighbourPos) neighbours.add(Seat(neighbourPos, getValueOfPosition(neighbourPos)))
             }
         }
         return neighbours
     }
 
-    private fun getSeat(pos: Position): Char {
+    fun findSeatsThatCanBeSeen(pos: Position): Set<Seat> {
+        return listOfNotNull(
+            seeInDirection(-1, -1, pos), seeInDirection(0, -1, pos), seeInDirection(1, -1, pos),
+            seeInDirection(-1, 0, pos), seeInDirection(1, 0, pos),
+            seeInDirection(-1, 1, pos), seeInDirection(0, 1, pos), seeInDirection(1, 1, pos)
+        )
+            .map { Seat(it, getValueOfPosition(it)) }
+            .toSet()
+    }
+
+    private tailrec fun seeInDirection(incX: Int, incY: Int, from: Position): Position? {
+        val newPos = Position(from.x + incX, from.y + incY)
+        if (isOutOfBounds(newPos)) return null
+        if (floor[newPos.y][newPos.x] == Occupied) return newPos
+        if (floor[newPos.y][newPos.x] == EmptySeat) return null
+        return seeInDirection(incX, incY, newPos)
+    }
+
+    private fun isOutOfBounds(pos: Position): Boolean {
+        return pos.x < 0 || pos.x == floor.first().size || pos.y < 0 || pos.y == floor.size
+    }
+
+    private fun getValueOfPosition(pos: Position): Char {
         return floor[pos.y][pos.x]
     }
 
-    private fun printCurrentState() {
-        println()
-        println()
-        println()
-        floor.forEach {
-            println(it)
+    fun occupyOnly(seatsToBeOccupied: Set<Seat>) {
+        val positionsToBeOccupied = seatsToBeOccupied.map { it.position }
+        val newFloor = floor.mapIndexed { y, row ->
+            row.mapIndexed { x, seatValue ->
+                when {
+                    seatValue == Floor -> Floor
+                    positionsToBeOccupied.contains(Position(x, y)) -> Occupied
+                    else -> EmptySeat
+                }
+            }.toCharArray()
         }
-        println()
-        println()
-        println()
+        floor = newFloor
     }
+
+    fun getNumberOfOccupiedSeats(): Int {
+        return floor.map { it.filter { it == Occupied }.size }.sum()
+    }
+
 }
 
+val shouldBeOccupiedPart1 = fun(seat: Seat, waitingArea: WaitingArea): Boolean {
+    val currentValue = seat.value
+    if (currentValue == EmptySeat && (calculateOccupiedNeighbours(seat.position, waitingArea) == 0)) return true
+    if (currentValue == Occupied && calculateOccupiedNeighbours(seat.position, waitingArea) < 4) return true
+    return false
+}
+
+private fun calculateOccupiedNeighbours(seatPos: Position, waitingArea: WaitingArea): Int {
+    return waitingArea.findNeighbours(seatPos)
+        .filter { it.value == Occupied }
+        .size
+}
+
+val shouldBeOccupiedPart2 = fun(seat: Seat, waitingArea: WaitingArea): Boolean {
+    val currentValue = seat.value
+    val seatsSeen = waitingArea.findSeatsThatCanBeSeen(seat.position)
+    if (currentValue == EmptySeat && seatsSeen.isEmpty()) return true
+    if (currentValue == Occupied && seatsSeen.size < 5) return true
+    return false
+}
+
+data class Seat(val position: Position, val value: Char)
 typealias NumberOfChanges = Int
 
 const val Floor = '.'
--- src/test/kotlin/day/Day11Test.kt
+++ src/test/kotlin/day/Day11Test.kt
@@ -4,6 +4,7 @@ import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import readInput
 
 internal class Day11Test {
 
@@ -23,6 +24,23 @@ internal class Day11Test {
     @Test
     fun `From example 1`() {
         Day11(exampleInput.lines()).calculateFirst() shouldBe 37
+    }
+
+    @Test
+    fun `From example 2`() {
+        Day11(exampleInput.lines()).calculateSecond() shouldBe 26
+    }
+
+    @Test
+    fun `Correct answer after turning in 1`() {
+        val solution = Day11(readInput(11)).calculateFirst()
+        solution shouldBe 2324
+    }
+
+    @Test
+    fun `Correct answer after turning in 2`() {
+        val solution = Day11(readInput(11)).calculateSecond()
+        solution shouldBe 2068
     }
 
 }
